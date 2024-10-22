65d75c88b48c0a517de1d58889f03d617d7cf19e
Mehdi <mehdi.teisseire@laplateforme.io>
2024-10-22 12:57:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-
-def print_tic_tac_toe(a,b):
+def print_tic_tac_toe(a):
     print("\n")
     print ("\t     |     |")
-    print (f"\t {a}   |  {b}  |  {a}")
+    print (f"\t {a[0]}   |  {a[1]}  |  {a[2]}")
     print('\t_____|_____|_____')
  
     print( "\t     |     |")
-    print (f"\t {a}   |  {a}  |  {b}")
+    print (f"\t {a[3]}   |  {a[4]}  |  {a[5]}")
     print ('\t_____|_____|_____')
  
     print("\t     |     |")
  
-    print (f"\t {a}   |  {b}  |  {b}")
+    print (f"\t {a[6]}   |  {a[7]}  |  {a[8]}")
     print("\t     |     |")
     print("\n")
 
